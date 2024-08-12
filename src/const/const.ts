6d097c935debd29d97b34d084107064cee1939a5
RolandoDev <rolando.mizhquiri@gmail.com>
2024-08-12 01:03:15 -0500

    adding the first files from the repository

---
+++ src/const/const.ts
@@ -0,0 +1,57 @@
+import { title } from "process"
+
+export const IconKeys = {
+    html:"html",
+    css:"css",
+    javaScript:"javaScript",
+    react:"react",
+    astro:"astro",
+    typeScript:"typeScript",
+    git:"git",
+    github:"github",
+    postgress:"postgress",
+    springboot:"springboot"
+}
+export const skills = [
+    {
+        title:'Html',
+        icon:IconKeys.html
+    },
+    {
+        title:'Css',
+        icon:IconKeys.css
+    },
+    {
+        title:'JavaScript',
+        icon:IconKeys.javaScript
+    },
+    {
+        title:'React',
+        icon:IconKeys.react
+    },
+    {
+        title:'Astro',
+        icon:IconKeys.astro
+    },
+    {
+        title:'TypeScript',
+        icon:IconKeys.typeScript
+    },
+    {
+        title:'Git',
+        icon:IconKeys.git
+    },
+    {
+        title:'Github',
+        icon:IconKeys.github
+    },
+    {
+        title: 'PostgreSQL',
+        icon:IconKeys.postgress
+    },
+    {
+        title:'Spring Boot',
+        icon:IconKeys.springboot
+    }
+
+]
